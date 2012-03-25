ace81c2294b009534afb1a7cef0e5f3b42145ca4
Noam <noam.rubin@students.olin.edu>
2012-03-25 13:46:20 -0400

    commented code

--- Asker.py
+++ Asker.py
@@ -1,23 +1,36 @@
 #Pull in CGQ and CGA files, correspond Q and As, and random ask questions and match them to answers
 
 def openQuestions():
+    """
+    Opens the file with questions in it
+    """
     f = open('CGQ.txt')
     text = f.read()
     f.close()
     return text.split('\n')
     
-def openAnswers():    
+def openAnswers():
+    """
+    Opens the file with answers in it
+    """    
     f = open('CGA.txt')
     text = f.read()
     f.close()
     return text.split('\n')
     
 def QAPairs():
+    """
+    Opens the question and answer files and zips the pairs together
+    """
     qs = openQuestions()
     ans = openAnswers()
     return zip(qs,ans)
     
 def askQuestion(num):
+    """
+    This does the work. It takes the number of questions you want, and
+    randomly selects that many question and answer pairs and asks you them.
+    """
     import random
     pairs = QAPairs()
     pairsToAsk = random.sample(pairs,num)
